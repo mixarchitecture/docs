d58e921db57106e982f010a8d97b52d587a72f6b
Sami Salih İbrahimbas <info@ssibrahimbas.com>
2023-01-30 23:38:36 +0300

    ci-cd: initial setup

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,8 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'mixarchitecture', // Usually your GitHub org/user name.
