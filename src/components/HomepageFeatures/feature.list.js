0e1f13bf104fe9e07d370a91711377f5e36c0fe6
Sami Salih İbrahimbas <info@ssibrahimbas.com>
2023-02-01 21:15:59 +0300

    dev: add tr index page

---
+++ src/components/HomepageFeatures/feature.list.js
@@ -0,0 +1,166 @@
+import React from "react";
+
+export const FeatureList = {
+    en: [
+        {
+            title: 'Decompose, Decompose thoroughly!',
+            Svg: require('@site/static/img/1.svg').default,
+            description: (
+                <>
+                    Mix Architecture is a modular architecture. It is a modular architecture that is easy to understand and easy to maintain.
+                </>
+            ),
+        },
+        {
+            title: 'Researched, The Best!',
+            Svg: require('@site/static/img/2.svg').default,
+            description: (
+                <>
+                    Mix Architecture is a blend of the best patterns and architectural solutions. It is the best of the best. Moreover language independent!
+                </>
+            ),
+        },
+        {
+            title: 'Test, Test, Test...',
+            Svg: require('@site/static/img/4.svg').default,
+            description: (
+                <>
+                    Mix Architecture makes your application independent with the architectures it uses. In this way, an easily testable structure is formed.
+                </>
+            ),
+        },
+    ],
+    tr: [
+        {
+            title: 'Ayrıştırın, İyice Ayrıştırın!',
+            Svg: require('@site/static/img/1.svg').default,
+            description: (
+                <>
+                    Mix Architecture modüler bir mimaridir. Anlaşılması kolay ve bakımı kolay modüler bir mimaridir.
+                </>
+            ),
+        },
+        {
+            title: 'Araştırıldı, En İyisi!',
+            Svg: require('@site/static/img/2.svg').default,
+            description: (
+                <>
+                    Mix Architecture, en iyi kalıpların ve mimari çözümlerin bir karışımıdır. En iyinin en iyisidir. Üstelik dilden bağımsız!
+                </>
+            ),
+        },
+        {
+            title: 'Test, Test, Test...',
+            Svg: require('@site/static/img/4.svg').default,
+            description: (
+                <>
+                    Mix Architecture, kullandığı mimariler ile uygulamanızı bağımsız hale getirir. Bu sayede kolayca test edilebilir bir yapı oluşur.
+                </>
+            ),
+        },
+    ]
+};
+
+export const TextFeatureList = {
+    tr: [
+        {
+            title: 'Ön Uç Desteklenir',
+            description: (
+                <>
+                    Mix Architecture, ön uç tarafından desteklenir. Ön uç alanına uygun mimari ve pattern bazlı çözümler sunar.
+                </>
+            ),
+        },
+        {
+            title: 'Açık Kaynak',
+            description: (
+                <>
+                  Mix Architecture açık kaynaklıdır. Yanlış yerleri bildirebilir, dileklerinizi iletebilir, topluluğa soru sorabilir veya ücretsiz olarak kullanabilirsiniz.
+                </>
+            ),
+        },
+        {
+            title: 'Gerçek Kullanım Örnekleri',
+            description: (
+                <>
+                    Mix Architecture, gerçek kullanım durumlarına sahiptir. Örnekler bölümünde Mix Architecture'ın gerçek kullanım örneklerini görebilir veya istediğiniz bir örneği isteyebilirsiniz.
+                </>
+            ),
+        },
+        {
+            title: 'Dilden Bağımsız',
+            description: (
+                <>
+                    Mix Architecture dilden bağımsızdır. İstediğiniz dilde kullanabilirsiniz. Belirli bir dil ile sınırlı değildir.
+                </>
+            ),
+        },
+        {
+            title: 'Kontrol Sizde',
+            description: (
+                <>
+                   Mix Architecture, sizin çerçeveler için değil, çerçevelerin sizin için çalışmasını sağlar. Kullandığınız çerçevelerin kontrolü sizdedir. 
+                </>
+            ),
+        },
+        {
+            title: 'Ne istersen',
+            description: (
+                <>
+                    Mix Architecture, bir paketin, protokolün veya herhangi bir şeyin mantığınıza müdahale etmesini önler ve onu ayrıştırır. Hatta birçoğunu aynı anda kullanabilirsiniz.
+                </>
+            ),
+        }
+    ],
+    en: [
+        {
+            title: 'Frontend Supported',
+            description: (
+                <>
+                    Mix Architecture is frontend supported. It offers architectural and pattern-based solutions suitable for the frontend area.
+                </>
+            ),
+        },
+        {
+            title: 'Open Source',
+            description: (
+                <>
+                    Mix Architecture is open source. You can report the wrong places, express your wishes, ask questions to the community or use it for free.
+                </>
+            ),
+        },
+        {
+            title: 'Real Use Cases',
+            description: (
+                <>
+                    Mix Architecture is have real use cases. You can see the real use cases of Mix Architecture in the Examples section or you can request an example of your choice.
+                </>
+            ),
+        },
+        {
+            title: 'Language Independent',
+            description: (
+                <>
+                    Mix Architecture is language independent. You can use it in any language you want. It is not limited to a specific language.
+                </>
+            ),
+        },
+        {
+            title: 'You Are in Control',
+            description: (
+                <>
+                    Mix Architecture ensures that frameworks work for you, not you for frameworks. You are in control of the frameworks you use.
+                </>
+            ),
+        },
+        {
+            title: 'Whatever You Want',
+            description: (
+                <>
+                    Mix Architecture prevents a packet, protocol or anything from interfering with your logic and parses it. You can even use many of them at the same time.
+                </>
+            ),
+        }
+    ]
+
+}
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,89 +1,9 @@
 import clsx from 'clsx';
 import React from 'react';
+import { FeatureList, TextFeatureList } from './feature.list';
 import styles from './styles.module.css';
 
-const FeatureList = [
-  {
-    title: 'Decompose, Decompose thoroughly!',
-    Svg: require('@site/static/img/1.svg').default,
-    description: (
-      <>
-        Mix Architecture is a modular architecture. It is a modular architecture that is easy to understand and easy to maintain.
-      </>
-    ),
-  },
-  {
-    title: 'Researched, The Best!',
-    Svg: require('@site/static/img/2.svg').default,
-    description: (
-      <>
-        Mix Architecture is a blend of the best patterns and architectural solutions. It is the best of the best. Moreover language independent!
-      </>
-    ),
-  },
-  {
-    title: 'Test, Test, Test...',
-    Svg: require('@site/static/img/4.svg').default,
-    description: (
-      <>
-        Mix Architecture makes your application independent with the architectures it uses. In this way, an easily testable structure is formed.
-      </>
-    ),
-  },
-];
-
-const TextFeatureList = [
-  {
-    title: 'Frontend Supported',
-    description: (
-      <>
-        Mix Architecture is frontend supported. It offers architectural and pattern-based solutions suitable for the frontend area.
-      </>
-    ),
-  },
-  {
-    title: 'Open Source',
-    description: (
-      <>
-        Mix Architecture is open source. You can report the wrong places, express your wishes, ask questions to the community or use it for free.
-      </>
-    ),
-  },
-  {
-    title: 'Real Use Cases',
-    description: (
-      <>
-        Mix Architecture is have real use cases. You can see the real use cases of Mix Architecture in the Examples section or you can request an example of your choice.
-      </>
-    ),
-  },
-  {
-    title: 'Language Independent',
-    description: (
-      <>
-        Mix Architecture is language independent. You can use it in any language you want. It is not limited to a specific language.
-      </>
-    ),
-  },
-  {
-    title: 'You Are in Control',
-    description: (
-      <>
-      Mix Architecture ensures that frameworks work for you, not you for frameworks. You are in control of the frameworks you use.
-      </>
-    ),
-  },
-  {
-    title: 'Whatever You Want',
-    description: (
-      <>
-      Mix Architecture prevents a packet, protocol or anything from interfering with your logic and parses it. You can even use many of them at the same time.
-      </>
-    ),
-  }
-]
-
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
@@ -97,7 +17,13 @@ function Feature({Svg, title, description}) {
   );
 }
 
-function TextFeature({title, description}) {
+function KeyFeatureTitle({ lang }) {
+  if (lang == "en") return "Key Features";
+  if (lang == "tr") return "Ana Özellikler";
+  return "Key Features";
+}
+
+function TextFeature({ title, description }) {
   return (
     <div className={clsx('col col--4', styles.textFeature)}>
       <div className="text--center">
@@ -108,19 +34,23 @@ function TextFeature({title, description}) {
   );
 }
 
-export default function HomepageFeatures() {
+export default function HomepageFeatures({ lang = en }) {
   return (
     <section className={styles.features}>
       <div className="container">
         <div className="row">
-          {FeatureList.map((props, idx) => (
+          {FeatureList[lang].map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
         </div>
         <div className={styles.divider}></div>
         <div className='row'>
-          <div className={clsx('col col--12', styles.title)}><h2 className='text--center'>Key Features</h2></div>
-          {TextFeatureList.map((props, idx) => (
+          <div className={clsx('col col--12', styles.title)}><h2 className='text--center'>
+            <KeyFeatureTitle
+            lang={lang}
+          />
+          </h2></div>
+          {TextFeatureList[lang].map((props, idx) => (
             <TextFeature key={idx} {...props} />
           ))}
         </div>
