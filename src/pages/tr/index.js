9149c3d14c0a4292deaa017b2bc94444cbd89c78
Sami Salih İbrahimbas <info@ssibrahimbas.com>
2023-02-01 22:21:32 +0300

    dev: update tr page redirect

--- src/pages/tr/index.js
+++ src/pages/tr/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/tr/docs/intro">
+            to="/docs/intro">
            Şimdi Başlayın &rarr;
           </Link>
         </div>
