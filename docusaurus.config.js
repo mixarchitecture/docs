e83013ceacf58bd6c32e6f4909bd9a05f6b27455
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-05-31 19:07:26 +0300

    config: update url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://.pages.dev",
+  url: "https://mixarchitecture.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
@@ -20,7 +20,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "mixarchitecture", // Usually your GitHub org/user name.
-  projectName: "arch", // Usually your repo name.
+  projectName: "docs", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
