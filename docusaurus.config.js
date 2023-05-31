d08c477089b7056da09d7857225d85f188bc9ce5
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-05-31 18:40:50 +0300

    config: update host url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,11 +11,11 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://mixarchitecture.github.io",
+  url: "https://https://mixarch-docs.pages.dev",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
-  deploymentBranch: "gh-pages",
+  deploymentBranch: "main",
   trailingSlash: false,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
